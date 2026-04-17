78eca6eea75e275289b611a5e306667ed269585e
Lucaodevel <lucas.martins.a@outlook.com>
2026-04-17 13:55:32 +0000

    Xadrez _ Nivel Avançado;

--- xadrez.c
+++ xadrez.c
@@ -2,11 +2,15 @@
 
 int main(){
 
-    // Declaração de váriaveis
+    // Declaração de variáveis
     const int movimentoTorre = 5;
     const int movimentoBispo = 5;
     const int movimentoRainha = 8;
 
+    // Declaração de variáveis
+    const int movimentoCavaloVertical = 2;   // Duas casas para baixo
+    const int movimentoCavaloHorizontal = 1; // Uma casa para a esquerda
+    
     // Movimentação da torre
     printf("Movimento da Torre:\n");
     for (int i = 1; i <= movimentoTorre; i++) {
@@ -32,12 +36,25 @@ int main(){
     } while (k <= movimentoRainha);
         printf("\n");
         k++;
+
+        // Movimentação do Cavalo
+        printf("Movimento do Cavalo\n");
+        for (int v = 1; v <= 1; v++) {
+            int cont_v = 0;
+            while (cont_v < movimentoCavaloVertical) {
+                printf("Baixo\n");
+                cont_v++;
+            }
+        int cont_h = 0;
+        while (cont_h < movimentoCavaloHorizontal) {
+            printf("Esquerda\n");
+            cont_h++;
+            }
+        }
+
+    printf("\n");
     return 0;
     }
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
